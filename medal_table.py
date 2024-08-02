128c526a6552cf295762c36d8fa0de5635129382
Aydin Arik <akyidrian@users.noreply.github.com>
2024-08-02 01:52:40 -0700

    Fixed scraping bug for the medal table
    Scrolling wasn't occuring properly during scraping of medal table from
    olympics site. We appeared to be just jumping to the end of the webpage,
    which is because we weren't properly looking at the current pageYOffset.
    This change also attempts to make things slightly more rebust by
    incrementally scrolling in viewport//5 steps and sleeping.

--- medal_table.py
+++ medal_table.py
@@ -1,17 +1,16 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.service import Service as FirefoxService
 import sqlite3
+import time
 from webdriver_manager.firefox import GeckoDriverManager
 
 
 DATABASE_NAME = 'medals.db'
+MEDAL_TABLE_URL = 'https://olympics.com/en/paris-2024/medals'
 
 
-def create_sqlite_table():
+def create_medals_table():
     conn = sqlite3.connect(DATABASE_NAME)
     cursor = conn.cursor()
     
@@ -45,53 +44,36 @@ def insert_or_update_data(order_number, flag_url, country_code, country_name, go
     conn.close()
 
 
-# TODO: Think there is a bug here
-def parse_table_from_visible_rows(html_content):
+def parse_table_from_visible_rows(content):
     '''
     Extracts visible medal table data from official olympics website
     and inserts the data into SQLite table
     '''
-    soup = BeautifulSoup(html_content, 'html.parser')
+    soup = BeautifulSoup(content, 'html.parser')
     rows = soup.find_all('div', {'data-testid': 'noc-row'})
     
     for row in rows:
         order_number = int(row.find('span', class_='e1oix8v91').text)
         flag_img = row.find('img', class_='elhe7kv3')
-        flag_url = flag_img['src'] if flag_img else 'N/A'
+        flag_url = flag_img['src'] if flag_img else ''
         country_code = row.find('span', class_='elhe7kv4').text
         country_name = row.find('span', class_='elhe7kv5').text
         medal_counts = row.find_all('span', class_='e1oix8v91 emotion-srm-81g9w1')
         gold = int(medal_counts[0].text) if len(medal_counts) > 0 else 0
         silver = int(medal_counts[1].text) if len(medal_counts) > 1 else 0
         bronze = int(medal_counts[2].text) if len(medal_counts) > 2 else 0
-        total_medals_span = row.find('span', class_='e1oix8v91 emotion-srm-5nhv3o')
-        total_medals = int(total_medals_span.text) if total_medals_span else 0
-
-        # Insert data into the SQLite table
+        total_medals = int(row.find('span', class_='e1oix8v91 emotion-srm-5nhv3o').text)
         insert_or_update_data(order_number, flag_url, country_code, country_name, gold, silver, bronze, total_medals)
 
-def create_table():
-    create_sqlite_table()
-    # Set up Firefox options
+
+def create_webdriver():
     options = webdriver.FirefoxOptions()
-
-    # Set user agent
     options.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-
-    # Run in headless mode
     options.add_argument("-headless")
-
-    # Set window size
     options.add_argument("--width=1920")
     options.add_argument("--height=1080")
-
-    # Disable GPU (not typically needed for Firefox, but included for completeness)
     options.add_argument("--disable-gpu")
-
-    # No sandbox (Firefox doesn't use sandboxing, so this is not applicable)
-
-    # Disable shared memory usage
-    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-shm-usage") # Disable shared memory usage
 
     # Proxy settings
     options.set_preference("network.proxy.type", 1)
@@ -99,42 +81,36 @@ def create_table():
     options.set_preference("network.proxy.socks_port", 0)
     options.set_preference("network.proxy.socks_remote_dns", False)
     install = GeckoDriverManager().install()
-    driver = webdriver.Firefox(service=FirefoxService(install), options=options)
+    return webdriver.Firefox(service=FirefoxService(install), options=options)
 
-    try:
-        # Open the URL
-        driver.get('https://olympics.com/en/paris-2024/medals')
 
-        # Wait for the initial content to load
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-testid="noc-row"]')))
+def create_table():
+    create_medals_table()
+    driver = create_webdriver()
 
-        # Scroll and extract data
-        last_height = driver.execute_script("return document.body.scrollHeight")
-        while True:
-            # Extract data from currently visible rows
-            html_content = driver.page_source
-            parse_table_from_visible_rows(html_content)
+    driver.get(MEDAL_TABLE_URL) # Open the url
+    time.sleep(1) # HACK: Wait for the initial content to load
 
-            # Scroll down by the height of the viewport
-            driver.execute_script("window.scrollBy(0, window.innerHeight);")
-            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-testid="noc-row"]')))
-            
-            # Check if we've reached the bottom of the page
-            new_height = driver.execute_script("return document.body.scrollHeight")
-            if new_height == last_height:
-                break
-            last_height = new_height
+    # Continuously scroll and extract data from webpage until we reach the end...
+    # HACK: We scroll downwards in viewport_height//5 increments and sleep for 100ms before
+    #       continuing the next incremental scroll
+    max_height = driver.execute_script("return document.body.scrollHeight")
+    viewport_height = driver.execute_script("return window.innerHeight")
+    last_height = driver.execute_script("return window.pageYOffset + window.innerHeight")
+    while last_height < max_height:
+        parse_table_from_visible_rows(driver.page_source)
 
-        # Ensure all content is loaded by scrolling to the bottom again and waiting
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[data-testid="noc-row"]')))
+        # Scroll down by a fraction of the height of the viewport
+        driver.execute_script(f"window.scrollBy(0, {viewport_height//5});")
+        time.sleep(0.1) # HACK: Ensure content has loaded
+        last_height = driver.execute_script("return window.pageYOffset + window.innerHeight")
 
-        # Final extraction to ensure we got everything
-        html_content = driver.page_source
-        parse_table_from_visible_rows(html_content)
+    # Ensure we wcroll to the bottom, sleep then parse any remaining content
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(0.1) # HACK: Ensure content has loaded
+    parse_table_from_visible_rows(driver.page_source)
 
-    finally:
-        driver.quit()
+    driver.quit()
 
 
 def print_all_rows():
