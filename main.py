960669782cc7bb776d4540674593bcaec603ef96
Aydin Arik <akyidrian@users.noreply.github.com>
2024-08-06 17:03:38 -0700

    Using fastlite.database instead of sqlite3
    Also:
    1. Fixed one bug around calling create_database in background task
       unnecessarily
    2. Made pop per medal calc more resilient to divide-by-zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fasthtml.fastapp import *
 import asyncio
+import fastlite
 import medal_table
 import population
 import sqlite3
@@ -32,15 +33,12 @@ def create_database():
         print(f'ERROR: Failed to create medals table: {e}')
         return
 
-    # TODO: Not exactly true that we're always creating the table...
-    #       Most of the time we're probably just updating the existing tables...
     print('SUCCESS: Created medal table database')
 
 
 async def background_task():
     while True:
         await asyncio.sleep(1800 + int(random.uniform(-600, 600))) # Sleep for somewhere around 20-40mins
-        create_database()
         try:
             medal_table.update_table()
         except sqlite3.Error as e:
@@ -63,45 +61,50 @@ def create_world_winners_row(query_rows):
     total = 0
     pop = 0
     for r in query_rows:
-        gold += r[4]
-        silver += r[5]
-        bronze += r[6]
-        total += r[7]
-        pop += r[8]
+        gold += r['gold']
+        silver += r['silver']
+        bronze += r['bronze']
+        total += r['total_medals']
+        pop += r['population']
 
     world_winners = Div(Img(src=OLYMPIC_RINGS_URL, alt="", width="20px"), 
                         Span("World Winners"))
     population = format(pop, ',')
-    population_per_medal = format(pop//total, ',')
+    population_per_medal = format(pop//total, ',') if total > 0 else '0'
     world_winners_row = map(Td, (0, world_winners, gold, silver, bronze, total, population, population_per_medal))
     return Tr(*world_winners_row)
 
 
 def create_country_row(query_row):
-        order, flag_url, country_code, country_name, gold, silver, bronze, total, pop = query_row
+        order = query_row['order_number']
+        flag_url = query_row['flag_url']
+        country_name = query_row['country_name']
+        country_code = query_row['country_code']
+        gold = query_row['gold']
+        silver = query_row['silver']
+        bronze = query_row['bronze']
+        total = query_row['total_medals']
+        pop = query_row['population']
         country = Div(Img(src=flag_url, alt="", width="20px"),
                       Span(f"{country_name} ({country_code})"))
         population = format(pop, ',')
-        population_per_medal = format(pop//query_row[7], ',')
+        population_per_medal = format(pop//total, ',')
         country_row = map(Td, (order, country, gold, silver, bronze, total, population, population_per_medal))
         return Tr(*country_row)
 
 
-# TODO: Maybe there is a more Fast HTML way instead of using sqlite3?
 def create_medal_table():
-    conn = sqlite3.connect(medal_table.DATABASE_NAME)
-    cursor = conn.cursor()
-    cursor.execute('''
+    db = fastlite.database(medal_table.DATABASE_NAME)
+    query = '''
         SELECT m.*,
                COALESCE(p1.population, p2.population, 0) AS population
         FROM medals m
         LEFT JOIN population p1 ON m.country_name = p1.entity
         LEFT JOIN population p2 ON m.country_code = p2.code
-    ''')
-    query_rows = cursor.fetchall()
-    conn.close()
+    '''
+    query_rows = db.q(query)
 
-    sorted_query_rows = sorted(query_rows, key=lambda r: (r[0], r[3]), reverse=False)
+    sorted_query_rows = sorted(query_rows, key=lambda r: (r['order_number'], r['country_name']), reverse=False)
     table_rows = [create_world_winners_row(sorted_query_rows)] # World Winners will always be number 1 (rank 0)
     for r in sorted_query_rows:
         table_rows.append(create_country_row(r))
